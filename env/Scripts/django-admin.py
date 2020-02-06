b6ce009a0df12a9fd8be999aa1830728c7092880
hzw <1173577252@qq.com>
2020-02-06 10:41:08 +0800

    upload

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!g:\django_blog_tutorial-master\django_blog_tutorial-master\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
