f566d7784b1aa432713dd6afd072d37a9f5ffec9
hzw <1173577252@qq.com>
2020-02-20 13:24:38 +0800

    delete

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!g:\django_blog_tutorial-master\django_blog_tutorial-master\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
